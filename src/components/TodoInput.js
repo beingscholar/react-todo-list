289b45a831f082bfb8eb1b0e97913286fd48e960
Mayank Agarwal <mayankagarwal@Xavients-MacBook-Pro.local>
2020-04-13 01:55:45 +0530

    first commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class TodoInput extends Component {
+  render() {
+    return (
+      <div>
+        Todo Input
+        <input type='text' name='' id='' />
+      </div>
+    );
+  }
+}
